f4ebe74b766710dc0febf76c283cf95ab755e01b
wdd <296151852@qq.com>
2020-10-10 14:00:06 +0800

    第一次

--- src/views/about.jsx
+++ src/views/about.jsx
@@ -6,6 +6,7 @@ const title= '夜灵犀传奇之邪剑黑曜'
 const m1='我的马儿子，我永远是你爹爹！'
 const m2='---六耳'
 const title1= '简介：这江湖沉寂了二十余年，终于被一位少年打破！'
+const chapter= '第一回 群雄并起'
 const title2= '十年前。。。'
 const p1= '夜灵犀说：爱你在心口难开！'
 const p2= '苏晚说：那就给老娘闭嘴！'
@@ -24,6 +25,7 @@ export default class About extends React.Component {
               <div>{m1}&nbsp;&nbsp;&nbsp;&nbsp;{m2}</div>
               <p>{title1}</p>
             </div>
+            <div className='about-chapter'>{chapter}</div>
             <div className='about-cen'>
               <div className='about-model centre'>
                 <p>{title2}</p>
